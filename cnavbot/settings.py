20f435b1cd7d296293fd8f797ce9000de869c3e4
konradko <konrad.korzel@gmail.com>
2016-08-03 23:58:19 +0100

    Add default bot speed env var

--- cnavbot/settings.py
+++ cnavbot/settings.py
@@ -15,6 +15,6 @@ else:
     BOT_DRIVER = None
 
 # Must be between 0 and 100
-BOT_DEFAULT_SPEED = os.getenv('BOT_DEFAULT_SPEED', 50)
+BOT_DEFAULT_SPEED = os.getenv(int('BOT_DEFAULT_SPEED'), 50)
 BOT_DEFAULT_NAME = os.getenv('BOT_DEFAULT_NAME', HOSTNAME)
 
