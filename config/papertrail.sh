10da29f63ebc333d364fb01596e8e09159f7f817
konradko <konrad.korzel@gmail.com>
2016-08-26 22:31:35 +0100

    Fix papertrail config

--- config/papertrail.sh
+++ config/papertrail.sh
@@ -2,10 +2,10 @@
 
 mkdir -p /data/log/
 
-sed -i -e "s/host:/host: $PAPERTRAIL_HOST/g" \
--e "s/port:/port: $PAPERTRAIL_PORT/g" \
--e "s#BOT_LOG_PATH#$BOT_LOG_PATH#g" /etc/log_files.yml
-sed -i "s/host port/$PAPERTRAIL_HOST $PAPERTRAIL_PORT/g" /etc/systemd/system/papertrail.service
+sed -i -e "s/PAPERTRAIL_HOST/$PAPERTRAIL_HOST/" \
+-e "s/PAPERTRAIL_PORT/$PAPERTRAIL_PORT/" \
+-e "s#BOT_LOG_PATH#$BOT_LOG_PATH#" /etc/log_files.yml
+sed -i "s/PAPERTRAIL_HOST PAPERTRAIL_PORT/$PAPERTRAIL_HOST $PAPERTRAIL_PORT/" /etc/systemd/system/papertrail.service
 
 systemctl enable /etc/systemd/system/papertrail.service
 systemctl start papertrail.service
