83d4bbdf6d81977d899abafa80ed2715018032b2
konradko <konrad.korzel@gmail.com>
2016-09-10 00:43:54 +0100

    Make running papertrail, local ssh and prometheus optional with env vars

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo "Starting Papertrail"
-bash /usr/src/app/config/papertrail.sh
-
-echo "Starting OpenSSH"
-bash /usr/src/app/config/openssh.sh
-
 echo "Enabling Picamera"
 modprobe bcm2835-v4l2
 
@@ -15,7 +9,19 @@ modprobe i2c-dev
 echo "Enabling bluetooth"
 bash /usr/src/app/config/bluetooth.sh
 
-echo "Starting Prometheus"
-bash /usr/src/app/config/prometheus.sh
+case "$PAPERTRAIL_ON" in
+ true) bash /usr/src/app/config/papertrail.sh ;;
+    *) echo "Papertrail not enabled" ;;
+esac
+
+case "$LOCAL_SSH_ON" in
+ true) bash /usr/src/app/config/openssh.sh ;;
+    *) echo "Local SSH not enabled" ;;
+esac
+
+case "$PROMETHEUS_ON" in
+ true) bash /usr/src/app/config/prometheus.sh ;;
+    *) echo "Prometheus not enabled" ;;
+esac
 
 python cnavbot/main.py
